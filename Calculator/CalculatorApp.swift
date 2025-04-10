d7464b790f231eff31649c4702921f3921e6247b
OptimusPrime <gulliverraed@gmail.com>
2025-04-10 16:38:49 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Gulliver Raed on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
